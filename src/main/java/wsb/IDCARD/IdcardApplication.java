d133a4d41580016a6b2258ccc7f3eb8d2ed3e879
zowiedrid <ilham.fadhilah.ft20@mail.umy.ac.id>
2022-11-15 20:39:22 +0700

    start git

---
+++ src/main/java/wsb/IDCARD/IdcardApplication.java
@@ -0,0 +1,13 @@
+package wsb.IDCARD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/IDCARD/IdcardApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.IDCARD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
